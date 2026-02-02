b4f15c9b92b68fa011929e2f1b0f637dfdd374b3
Paulo Vitor Barbosa <40838575+pv08@users.noreply.github.com>
2026-02-02 08:21:18 -0300

    clients_per_round

--- generate_simulation.py
+++ generate_simulation.py
@@ -94,7 +94,7 @@ def main():
     parser.add_argument('--loc', type=str, default='austin', help="[austin, california, newyork, puertorico]")
     parser.add_argument('--host', type=str, default="0.0.0.0")
     parser.add_argument('--port', type=int, default=65432)
-    parser.add_argument('--clients_per_round', type=int, default=3)
+    parser.add_argument('--clients_per_round', type=int, default=5)
     parser.add_argument('--max_rounds', type=int, default=3)
     parser.add_argument('--epochs', type=int, default=15)
     parser.add_argument('--batch_size', type=int, default=128)
