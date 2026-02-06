2b75575de61227e22c26db33b1279784b46cb639
Paulo Vitor Barbosa <40838575+pv08@users.noreply.github.com>
2026-02-06 10:15:56 -0300

    final_commit_development

--- src/utils/gpu_lock.py
+++ src/utils/gpu_lock.py
@@ -2,35 +2,37 @@ import fcntl
 import os
 import time
 from logging import INFO, WARNING
+
+from src.utils.functions import mkdir_if_not_exists
 from src.utils.logger import log
 
 
 class GPULock:
-    def __init__(self, client_id, lock_file="/app/lock_dir/gpu.lock"):
-        self.lock_file = lock_file
+    def __init__(self, client_id, slots: int=1):
         self.client_id = client_id
-        os.makedirs(os.path.dirname(self.lock_file), exist_ok=True)
-        self.handle = None
+        self.slots = slots
+        mkdir_if_not_exists("lock_dir/")
+        self.lock_files = [f"app/lock_dir/gpu_{i}.lock" for i in range(slots)]
+        self.active_handle = None
+        self.active_slot = None
 
     def __enter__(self):
-        log(INFO, f"[Client {self.client_id}] Requesting GPU Lock... (Waiting in queue)")
-
-        self.handle = open(self.lock_file, "w")
-
-        # Bloqueia até conseguir
-        start_wait = time.time()
-        fcntl.flock(self.handle, fcntl.LOCK_EX)
-        wait_time = time.time() - start_wait
-
-        log(INFO, f"[Client {self.client_id}] GPU Lock ACQUIRED after {wait_time:.2f}s. Starting protected task.")
-        return self
+        while True:
+            for i in range(self.slots):
+                handle = open(self.lock_files[i], "w")
+                try:
+                    # Tenta o lock sem bloquear o processo inteiro (LOCK_NB)
+                    fcntl.flock(handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                    self.active_handle = handle
+                    self.active_slot = i
+                    return self
+                except BlockingIOError:
+                    handle.close()
+                    continue
+            time.sleep(1.0)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.handle:
-            try:
-                # Libera o lock
-                fcntl.flock(self.handle, fcntl.LOCK_UN)
-                self.handle.close()
-                log(INFO, f"[Client {self.client_id}] GPU Lock RELEASED.")
-            except Exception as e:
-                log(WARNING, f"[Client {self.client_id}] Error releasing lock: {e}")
+        if self.active_handle:
+            fcntl.flock(self.active_handle, fcntl.LOCK_UN)
+            self.active_handle.close()
+            log(INFO, f"[Client {self.client_id}] released slot {self.active_slot}.")
