2fa98f49fabad792ba98ad0cb189f36cea221fa8
Paulo Vitor Barbosa <40838575+pv08@users.noreply.github.com>
2026-02-06 10:04:53 -0300

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,11 +8,12 @@ DEFAULT_FORMATTER = logging.Formatter(
     "%(levelname)s %(name)s %(asctime)s | %(filename)s:%(lineno)d | %(message)s"
 )
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(DEFAULT_FORMATTER)
-LOGGER.addHandler(console_handler)
-
+if not LOGGER.handlers:
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(DEFAULT_FORMATTER)
+    LOGGER.addHandler(console_handler)
+    LOGGER.propagate = False
 
 logger = logging.getLogger(LOGGER_NAME)
 log = logger.log
