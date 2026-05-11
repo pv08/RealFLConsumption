d4caadd73d13daf2084e2210306979547883c0d1
Paulo Vitor Barbosa <40838575+pv08@users.noreply.github.com>
2026-05-11 09:57:25 -0300

    Claude enabled

--- migrate_to_numpy.sh
+++ migrate_to_numpy.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
-# Configura a localidade
-TARGET_LOC="austin"
+# Parse --loc argument
+TARGET_LOC=""
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --loc)
+            TARGET_LOC="$2"
+            shift 2
+            ;;
+        *)
+            echo "[!] - Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$TARGET_LOC" ]]; then
+    echo "[!] - Usage: bash $0 --loc <austin|ny|california|puerto_rico>"
+    exit 1
+fi
 
 echo "[!] - Initiating migration from CSV to Numpy"
 echo "[!] - Target location: $TARGET_LOC"
 
 # Lista de IDs (Copiada do seu entrypoint)
-ID_LIST=(8156 2335 9922 3039 4031 8386 7951 3538 9160 5746 1642 6139 2361 3456 9019 661 2818 7800 7536 4767 8565 4373 7901 7719 9278)
+AUSTIN_LIST=(8156 2335 9922 3039 4031 8386 7951 3538 9160 5746 1642 6139 2361 3456 9019 661 2818 7800 7536 4767 8565 4373 7901 7719 9278)
+NY_LIST=(1417 142 3000 3996 4550 2096 5058 387 2318 5997 5982 914 4283 3700 3517 3488 27 5587 1240 2358 558 5679 1222 950 9053)
+CALIFORNIA_LIST=(8342 7114 7062 6547 8733 9612 9836 9775 4495 9213 4934 2606 8061 3687 1731 3938 1450 1524 3864 203 5938 6377 8574)
+PUERTO_RICO_LIST=(8235 3835 7667 3987 11386 10887 6056 939 4046 11126 10826 1092 1286 1245 7778 2006 2159 11933 2921 8997 371 9911 4204 4749 6406)
+
+case "$TARGET_LOC" in
+    austin)       ID_LIST=("${AUSTIN_LIST[@]}") ;;
+    ny)           ID_LIST=("${NY_LIST[@]}") ;;
+    california)   ID_LIST=("${CALIFORNIA_LIST[@]}") ;;
+    puerto_rico)  ID_LIST=("${PUERTO_RICO_LIST[@]}") ;;
+    *)
+        echo "[!] - Unknown location '$TARGET_LOC'. Valid options: austin, ny, california, puerto_rico"
+        exit 1
+        ;;
+esac
 
 # Contadores para feedback visual
 TOTAL=${#ID_LIST[@]}
